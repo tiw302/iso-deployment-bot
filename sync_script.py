c2f35076f67d2b476a6a7950b8d970b1e0ac76ca
Jirawat Siripuk <siripukwaewwrrn@gmail.com>
2026-04-22 11:17:20 +0700

    fix: update iso database 2569-04-22 11:17

--- sync_script.py
+++ sync_script.py
@@ -82,15 +82,49 @@ def scrape_windows_eval():
 db = {
     "linux-distros/arch-based": [
         "https://mirror.kku.ac.th/archlinux/iso/latest/archlinux-x86_64.iso",
-        "https://mirror.cachyos.org/ISO/desktop/latest/cachyos-desktop-linux-x86_64-latest.iso"
+        "https://mirror.cachyos.org/ISO/desktop/latest/cachyos-desktop-linux-x86_64-latest.iso",
+        "https://iso.mirror.endeavouros.com/iso/latest/EndeavourOS-latest.iso",
+        "https://mirror.kku.ac.th/manjaro/kde/latest/manjaro-kde-latest.iso",
+        "https://garudalinux.org/downloads/garuda/dr460nized/garuda-dr460nized-linux-zen-latest.iso"
     ],
-    "linux-distros/independent": [
-        "https://repo-default.voidlinux.org/live/current/void-live-x86_64-musl-latest.iso"
+    "linux-distros/independent-and-minimal": [
+        "https://repo-default.voidlinux.org/live/current/void-live-x86_64-musl-latest.iso",
+        "https://repo-default.voidlinux.org/live/current/void-live-x86_64-latest.iso",
+        "https://dl-cdn.alpinelinux.org/alpine/latest-stable/releases/x86_64/alpine-standard-3.19.1-x86_64.iso",
+        "https://channels.nixos.org/nixos-23.11/latest-nixos-gnome-x86_64-linux.iso"
+    ],
+    "linux-distros/debian-based": [
+        "https://cdimage.debian.org/debian-cd/current/amd64/iso-dvd/debian-12.5.0-amd64-DVD-1.iso",
+        "https://mirror.kku.ac.th/ubuntu-releases/24.04/ubuntu-24.04-desktop-amd64.iso",
+        "https://mirror.kku.ac.th/ubuntu-releases/24.04/ubuntu-24.04-live-server-amd64.iso",
+        "https://mirror.kku.ac.th/linuxmint/stable/21.3/linuxmint-21.3-cinnamon-64bit.iso",
+        "https://iso.pop-os.org/22.04/amd64/intel/41/pop-os_22.04_amd64_intel_41.iso"
+    ],
+    "linux-distros/redhat-fedora": [
+        "https://mirror.kku.ac.th/fedora/linux/releases/40/Workstation/x86_64/iso/Fedora-Workstation-Live-x86_64-40-1.14.iso",
+        "https://mirror.kku.ac.th/fedora/linux/releases/40/Server/x86_64/iso/Fedora-Server-dvd-x86_64-40-1.14.iso",
+        "https://mirror.kku.ac.th/almalinux/9/isos/x86_64/AlmaLinux-9-latest-x86_64-dvd.iso",
+        "https://mirror.kku.ac.th/rocky/9/isos/x86_64/Rocky-9-latest-x86_64-dvd.iso"
+    ],
+    "security-pentest": [
+        "https://cdimage.kali.org/kali-2024.1/kali-linux-2024.1-installer-amd64.iso",
+        "https://deb.parrot.sh/parrot/iso/6.0/Parrot-security-6.0_amd64.iso",
+        "https://mirror.kku.ac.th/blackarch/iso/blackarch-linux-full-2023.12.01-x86_64.iso"
+    ],
+    "homelab-and-rescue": [
+        "https://enterprise.proxmox.com/iso/proxmox-ve_8.1-2.iso",
+        "https://download.truenas.com/TrueNAS-SCALE-23.10.2/TrueNAS-SCALE-23.10.2.iso",
+        "https://mirror.kku.ac.th/systemrescue/systemrescue-11.00-amd64.iso",
+        "https://github.com/HirenBootCD/HBCD_PE_x64/releases/download/v1.0.2/HBCD_PE_x64.iso"
     ],
     "massive-offline-packages": [
         "https://cdimage.kali.org/kali-2024.1/kali-linux-2024.1-everything-amd64.iso",
         "https://cdimage.debian.org/debian-cd/current/amd64/iso-bd/debian-mac-12.5.0-amd64-BD-1.iso",
-        "https://mirror.kku.ac.th/centos/8-stream/isos/x86_64/CentOS-Stream-8-x86_64-latest-dvd1.iso"
+        "https://cdimage.debian.org/debian-cd/current/amd64/iso-bd/debian-mac-12.5.0-amd64-BD-2.iso",
+        "https://cdimage.debian.org/debian-cd/current/amd64/iso-bd/debian-mac-12.5.0-amd64-BD-3.iso",
+        "https://cdimage.debian.org/debian-cd/current/amd64/iso-bd/debian-mac-12.5.0-amd64-BD-4.iso",
+        "https://mirror.kku.ac.th/centos/8-stream/isos/x86_64/CentOS-Stream-8-x86_64-latest-dvd1.iso",
+        "https://mirrors.edge.kernel.org/opensuse/distribution/leap/15.5/iso/openSUSE-Leap-15.5-DVD-x86_64-Media.iso"
     ]
 }
 
