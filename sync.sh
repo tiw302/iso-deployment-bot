224a513eb8d83a404bd49de8ca52b6ded6b4ef8e
Jirawat Siripuk <siripukwaewwrrn@gmail.com>
2026-05-24 17:27:33 +0700

    chore: update sync script execution flow

--- sync.sh
+++ sync.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# go to project directory
+# cd to project root
 cd "$(dirname "$0")" || exit
 
 HOSTNAME=$(hostname)
 TIMESTAMP=$(date +'%Y-%m-%d %H:%M')
 
-# check for changes in repository
+# check for repo changes
 if [[ -n $(git status -s) ]]; then
     echo "[*] changes detected, syncing..."
     git add .
     git commit -m "update from $HOSTNAME - $TIMESTAMP"
     
-    # pull latest and push changes
+    # pull rebase and push
     if git pull --rebase && git push; then
         echo "[/] sync done. ($HOSTNAME) /|\ ^._.^ /|\\"
     else
