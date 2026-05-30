16f14fa244ee069a9dbce1159705b5b6d25626bc
Jirawat Siripuk <siripukwaewwrrn@gmail.com>
2026-05-30 09:35:12 +0700

    feat: expand database with Mint, Pop!_OS, Zorin, and enterprise distros

--- src/distros.py
+++ src/distros.py
@@ -1,4 +1,4 @@
-# updated 2026-05
+# updated 2026-05-30
 
 DB: dict[str, list[dict]] = {
 
@@ -48,9 +48,31 @@ DB: dict[str, list[dict]] = {
         {"name": "SparkyLinux 8 LXQt", "url": "https://sourceforge.net/projects/sparkylinux/files/8.0/sparkylinux-8.0-x86_64-lxqt.iso/download", "size": "2.0GB"},
     ],
 
+    # linux mint
+    "linux/mint": [
+        {"name": "Linux Mint 22.3 Cinnamon", "url": "https://mirrors.kernel.org/linuxmint/stable/22.3/linuxmint-22.3-cinnamon-64bit.iso", "size": "2.8GB", "tags": ["lts", "desktop", "cinnamon", "ubuntu", "debian"], "docs": "https://linuxmint-user-guide.readthedocs.io", "notes": "The most popular Linux Mint edition with the Cinnamon desktop. Ideal for Windows switchers."},
+        {"name": "Linux Mint 22.3 XFCE", "url": "https://mirrors.kernel.org/linuxmint/stable/22.3/linuxmint-22.3-xfce-64bit.iso", "size": "2.6GB", "tags": ["lts", "desktop", "xfce", "ubuntu", "debian"]},
+        {"name": "Linux Mint 22.3 MATE", "url": "https://mirrors.kernel.org/linuxmint/stable/22.3/linuxmint-22.3-mate-64bit.iso", "size": "2.7GB", "tags": ["lts", "desktop", "mate", "ubuntu", "debian"]},
+    ],
+
+    # pop!_os
+    "linux/pop-os": [
+        {"name": "Pop!_OS 24.04 LTS", "url": "https://iso.pop-os.org/24.04/amd64/intel/6/pop-os_24.04_amd64_intel_6.iso", "size": "3.0GB", "tags": ["lts", "desktop", "cosmic", "ubuntu", "debian"], "docs": "https://support.system76.com", "notes": "Features the new COSMIC desktop. Great for developers and gamers."},
+        {"name": "Pop!_OS 24.04 LTS (NVIDIA)", "url": "https://iso.pop-os.org/24.04/amd64/nvidia/6/pop-os_24.04_amd64_nvidia_6.iso", "size": "3.2GB", "tags": ["lts", "desktop", "nvidia", "ubuntu", "debian"]},
+    ],
+
+    # zorin os
+    "linux/zorin": [
+        {"name": "Zorin OS 17.3 Core", "url": "https://sourceforge.net/projects/zorin-os/files/17/Zorin-OS-17.3-Core-64-bit.iso/download", "size": "3.4GB", "tags": ["lts", "desktop", "gnome", "ubuntu", "debian"], "docs": "https://help.zorin.com", "notes": "Designed to make Linux accessible to Windows and macOS users."},
+    ],
+
     # arch family
     "linux/arch-family": [
         {"name": "Arch Linux latest", "url": "https://mirror.kku.ac.th/archlinux/iso/latest/archlinux-x86_64.iso", "size": "1.1GB"},
+        {"name": "Manjaro KDE", "url": "https://download.manjaro.org/kde/25.0.3/manjaro-kde-25.0.3-250519-linux614.iso", "size": "3.8GB", "tags": ["rolling", "desktop", "kde", "arch"], "docs": "https://wiki.manjaro.org", "notes": "Arch-based, user-friendly rolling release with hardware detection."},
+        {"name": "Manjaro GNOME", "url": "https://download.manjaro.org/gnome/25.0.3/manjaro-gnome-25.0.3-250519-linux614.iso", "size": "3.5GB", "tags": ["rolling", "desktop", "gnome", "arch"]},
+        {"name": "Manjaro XFCE", "url": "https://download.manjaro.org/xfce/25.0.3/manjaro-xfce-25.0.3-250519-linux614.iso", "size": "3.3GB", "tags": ["rolling", "desktop", "xfce", "arch"]},
+        {"name": "EndeavourOS Endeavour", "url": "https://mirror.alpix.eu/endeavouros/iso/EndeavourOS_Endeavour-2025.03.19.iso", "size": "2.0GB", "tags": ["rolling", "desktop", "arch"], "docs": "https://discovery.endeavouros.com", "notes": "Terminal-centric Arch installer with a welcoming community."},
         {"name": "Garuda Dr460nized latest", "url": "https://iso.builds.garudalinux.org/iso/latest/garuda/dr460nized/latest.iso", "size": "3.0GB"},
         {"name": "Garuda GNOME latest", "url": "https://iso.builds.garudalinux.org/iso/latest/garuda/gnome/latest.iso", "size": "3.0GB"},
         {"name": "CachyOS KDE latest", "url": "https://mirror.cachyos.org/ISO/kde/latest/cachyos-kde-linux-latest.iso", "size": "3.1GB"},
@@ -61,6 +83,10 @@ DB: dict[str, list[dict]] = {
     # enterprise / rpm
     "linux/enterprise": [
         {"name": "Fedora 44 Workstation", "url": "https://dl.fedoraproject.org/pub/fedora/linux/releases/44/Workstation/x86_64/iso/Fedora-Workstation-Live-x86_64-44-1.1.iso", "size": "2.2GB"},
+        {"name": "Rocky Linux 9 Minimal", "url": "https://download.rockylinux.org/pub/rocky/9/isos/x86_64/Rocky-9-latest-x86_64-minimal.iso", "size": "1.8GB", "tags": ["server", "enterprise", "redhat"], "docs": "https://docs.rockylinux.org", "notes": "CentOS replacement. RHEL-compatible enterprise distribution."},
+        {"name": "Rocky Linux 9 DVD", "url": "https://download.rockylinux.org/pub/rocky/9/isos/x86_64/Rocky-9-latest-x86_64-dvd.iso", "size": "10.0GB", "tags": ["server", "enterprise", "redhat"]},
+        {"name": "AlmaLinux 9 Minimal", "url": "https://repo.almalinux.org/almalinux/9/isos/x86_64/AlmaLinux-9-latest-x86_64-minimal.iso", "size": "1.9GB", "tags": ["server", "enterprise", "redhat"], "docs": "https://wiki.almalinux.org", "notes": "Community-driven RHEL fork. Free and open forever."},
+        {"name": "AlmaLinux 9 DVD", "url": "https://repo.almalinux.org/almalinux/9/isos/x86_64/AlmaLinux-9-latest-x86_64-dvd.iso", "size": "10.0GB", "tags": ["server", "enterprise", "redhat"]},
         {"name": "openSUSE Leap 15.6 DVD", "url": "https://download.opensuse.org/distribution/leap/15.6/iso/openSUSE-Leap-15.6-DVD-x86_64-Media.iso", "size": "4.7GB"},
         {"name": "CentOS Stream 9 DVD", "url": "https://mirror.stream.centos.org/9-stream/BaseOS/x86_64/iso/CentOS-Stream-9-latest-x86_64-dvd1.iso", "size": "9.5GB"},
         {"name": "Oracle Linux 9.5 DVD", "url": "https://yum.oracle.com/ISOS/OracleLinux/OL9/u5/x86_64/OracleLinux-R9-U5-x86_64-dvd.iso", "size": "10.0GB"},
@@ -145,6 +171,9 @@ DB: dict[str, list[dict]] = {
         {"name": "Alpine Linux 3.21 Standard", "url": "https://dl-cdn.alpinelinux.org/alpine/v3.21/releases/x86_64/alpine-standard-3.21.3-x86_64.iso", "size": "215MB", "tags": ["minimal", "security", "container"], "docs": "https://wiki.alpinelinux.org", "notes": "Extremely compact security-oriented distro. Ideal for minimal VM instances."},
         {"name": "Alpine Linux 3.21 Extended", "url": "https://dl-cdn.alpinelinux.org/alpine/v3.21/releases/x86_64/alpine-extended-3.21.3-x86_64.iso", "size": "625MB"},
         {"name": "Alpine Virt", "url": "https://dl-cdn.alpinelinux.org/alpine/v3.21/releases/x86_64/alpine-virt-3.21.3-x86_64.iso", "size": "210MB"},
+        {"name": "Void Linux XFCE", "url": "https://repo-default.voidlinux.org/live/current/void-live-x86_64-xfce-musl-20250202.iso", "size": "850MB", "tags": ["rolling", "minimal", "independent"], "docs": "https://docs.voidlinux.org", "notes": "Independent rolling-release distro with runit init system and XBPS package manager."},
+        {"name": "Void Linux Base", "url": "https://repo-default.voidlinux.org/live/current/void-live-x86_64-20250202-base.iso", "size": "550MB", "tags": ["rolling", "minimal", "independent"]},
+        {"name": "Gentoo Minimal Install", "url": "https://distfiles.gentoo.org/releases/amd64/autobuilds/current-install-amd64-minimal/install-amd64-minimal-20260524T170407Z.iso", "size": "700MB", "tags": ["source-based", "minimal", "rolling"], "docs": "https://wiki.gentoo.org/wiki/Handbook:AMD64", "notes": "Source-based meta-distribution. Maximum customization through compiling from source."},
     ],
 
     # homelab
@@ -158,8 +187,14 @@ DB: dict[str, list[dict]] = {
         {"name": "Harvester HCI v1.4", "url": "https://releases.rancher.com/harvester/v1.4.0/harvester-v1.4.0-amd64.iso", "size": "4.6GB"},
     ],
 
+    # firewall / router
+    "homelab/firewall": [
+        {"name": "OPNsense 25.1 DVD", "url": "https://mirror.ams1.nl.leaseweb.net/opnsense/releases/25.1/OPNsense-25.1-dvd-amd64.iso.bz2", "size": "1.4GB", "tags": ["firewall", "router", "bsd", "homelab"], "docs": "https://docs.opnsense.org", "notes": "Modern FreeBSD-based firewall platform. Feature-rich web GUI for routing, VPN, and IDS/IPS."},
+    ],
+
     # nas
     "homelab/nas": [
+        {"name": "TrueNAS SCALE", "url": "https://download.truenas.com/TrueNAS-SCALE-Dragonfish/25.10.3.1/TrueNAS-SCALE-25.10.3.1.iso", "size": "1.3GB", "tags": ["nas", "zfs", "homelab"], "docs": "https://www.truenas.com/docs/scale/", "notes": "Enterprise-grade ZFS NAS. Supports VMs, containers, and apps out of the box."},
         {"name": "OpenMediaVault 7.4", "url": "https://sourceforge.net/projects/openmediavault/files/7.4.7/openmediavault_7.4.7-amd64.iso/download", "size": "1.1GB"},
         {"name": "Rockstor 5.0", "url": "https://sourceforge.net/projects/rockstor/files/5.0.15-0/Rockstor-5.0.15-0-x86_64.iso/download", "size": "2.2GB"},
     ],
@@ -177,6 +212,7 @@ DB: dict[str, list[dict]] = {
 
     # recovery & rescue tools
     "recovery/tools": [
+        {"name": "GParted Live 1.8.1", "url": "https://sourceforge.net/projects/gparted/files/gparted-live-stable/1.8.1-3/gparted-live-1.8.1-3-amd64.iso/download", "size": "560MB", "tags": ["recovery", "partitioning"], "docs": "https://gparted.org/documentation.php", "notes": "Industry-standard graphical partition editor. Essential homelab utility."},
         {"name": "CloneZilla 3.2.0", "url": "https://sourceforge.net/projects/clonezilla/files/clonezilla_live_stable/3.2.0-5/clonezilla-live-3.2.0-5-amd64.iso/download", "size": "490MB"},
         {"name": "Ultimate Boot CD 5.3.9", "url": "https://sourceforge.net/projects/ubcd/files/ubcd/5.3.9/ubcd539.iso/download", "size": "960MB"},
         {"name": "Hiren's BootCD PE x64", "url": "https://www.hirensbootcd.org/files/HBCD_PE_x64.iso", "size": "2.7GB"},
@@ -208,6 +244,9 @@ DB: dict[str, list[dict]] = {
 
     # bsd / alternative
     "alternative/bsd": [
+        {"name": "FreeBSD 14.4 amd64 DVD", "url": "https://download.freebsd.org/releases/amd64/amd64/ISO-IMAGES/14.4/FreeBSD-14.4-RELEASE-amd64-dvd1.iso", "size": "4.2GB", "tags": ["bsd", "server", "stable"], "docs": "https://docs.freebsd.org", "notes": "Enterprise-class UNIX operating system. Known for advanced networking, security, and ZFS."},
+        {"name": "FreeBSD 14.4 amd64 Bootonly", "url": "https://download.freebsd.org/releases/amd64/amd64/ISO-IMAGES/14.4/FreeBSD-14.4-RELEASE-amd64-bootonly.iso", "size": "420MB", "tags": ["bsd", "netinst"]},
+        {"name": "OpenBSD 7.7 amd64", "url": "https://cdn.openbsd.org/pub/OpenBSD/7.7/amd64/install77.iso", "size": "600MB", "tags": ["bsd", "security"], "docs": "https://www.openbsd.org/faq/", "notes": "Security-focused BSD. Proactive security features, integrated cryptography, and clean code."},
         {"name": "NetBSD 10.1 amd64", "url": "https://ftp.netbsd.org/pub/NetBSD/NetBSD-10.1/images/NetBSD-10.1-amd64.iso", "size": "560MB"},
     ],
 
